7d10b4facde2501a97d2ed1c364d783be9444ba6
adimerpaul <adimer101@gmail.com>
2022-09-30 22:04:38 -0400

    modificado para revicion ruiz

--- front/src/pages/Producer.vue
+++ front/src/pages/Producer.vue
@@ -108,13 +108,14 @@
         <div class="row">
           <div class="col-6"><q-input dense outlined v-model="producer.nombres" label="NOMBRES(S)" required /></div>
           <div class="col-6"><q-input dense outlined v-model="producer.apellidos" label="APELLIDOS" /></div>
-          <div class="col-4"><q-input dense outlined v-model="producer.ci" label="CI" /></div>
+          <div class="col-2"><q-input dense outlined v-model="producer.ci" label="CI" /></div>
+          <div class="col-2"><q-select dense outlined v-model="departamento" :options="departamentos" label="EXPEDIDO" /></div>
           <div class="col-4"><q-input dense outlined v-model="producer.edad" label="EDAD" /></div>
           <div class="col-4"><q-input dense outlined v-model="producer.nroFamilia" label="NO FAMILIA" /></div>
-          <div class="col-3"><q-input dense outlined v-model="producer.masculino" label="MASCULINO" /></div>
-          <div class="col-3"><q-input dense outlined v-model="producer.femenino" label="FEMENINO" /></div>
-          <div class="col-3"><q-input dense outlined v-model="producer.varones" label="VARONES" /></div>
-          <div class="col-3"><q-input dense outlined v-model="producer.mujeres" label="MUJERES" /></div>
+          <div class="col-3"><q-input dense outlined v-model="producer.masculino" label="MASCULINO" v-if="producer.nroFamilia!=undefined && producer.nroFamilia!=null && producer.nroFamilia!=''" /></div>
+          <div class="col-3"><q-input dense outlined v-model="producer.femenino" label="FEMENINO" v-if="producer.nroFamilia!=undefined && producer.nroFamilia!=null && producer.nroFamilia!=''" /></div>
+          <div class="col-3"><q-input dense outlined v-model="producer.varones" label="VARONES" v-if="producer.nroFamilia!=undefined && producer.nroFamilia!=null && producer.nroFamilia!=''" /></div>
+          <div class="col-3"><q-input dense outlined v-model="producer.mujeres" label="MUJERES" v-if="producer.nroFamilia!=undefined && producer.nroFamilia!=null && producer.nroFamilia!=''" /></div>
           <div class="col-6"><q-input dense outlined v-model="producer.celular" label="CELULAR" /></div>
           <div class="col-6"><q-input dense outlined v-model="producer.provincia" label="PROVINCIA" /></div>
           <div class="col-6"><q-input dense outlined v-model="producer.departamento" label="1 DEPARTAMENTO" /></div>
@@ -126,9 +127,10 @@
           <div class="col-12"><q-input dense outlined v-model="producer.maquinaria" label="7 MAQUINARIA Y HERRAMIENTAS EMPLEADAS" /></div>
           <div class="col-12"><q-input dense outlined v-model="producer.riego" label="8 TIPO DE RIEGO AL QUE TIENE ACCESO" /></div>
           <div class="col-12"><q-input dense outlined v-model="producer.quinua" label="9 CANTIDAD DE QUINUA RECOLECTADA POR HECTAREA" /></div>
-          <div class="col-12"><q-input dense outlined v-model="producer.asociacion" label="10 PERTENECE A ALGUNA ASOCIACIÓN?" /></div>
-          <div class="col-6"><q-input dense outlined v-model="producer.si" label="SI" /></div>
-          <div class="col-6"><q-input dense outlined v-model="producer.no" label="NO" /></div>
+          <div class="col-6"><q-select :options="['SI','NO']" dense outlined v-model="producer.asociacion" label="10 PERTENECE A ALGUNA ASOCIACIÓN?" /></div>
+          <div class="col-6"><q-input v-if="producer.asociacion=='SI'" dense outlined v-model="producer.si" label="NOMBRE DE LA ASOCIACION" /></div>
+<!--          <div class="col-6"><q-input dense outlined v-model="producer.si" label="SI" /></div>-->
+<!--          <div class="col-6"><q-input dense outlined v-model="producer.no" label="NO" /></div>-->
           <div class="col-12"><q-input dense outlined v-model="producer.mejorar" label="11 COMO CREE QUE PODRÍA MEJORAR SU PRODUCCIÓN" /></div>
           <div class="col-12 text-center flex flex-center">
             <q-uploader
@@ -264,6 +266,18 @@ export default {
       producerUpdateDialog:false,
       loading:false,
       user:{},
+      departamento:{value:'OR',label:'Oruro'},
+      departamentos:[
+        {value:'CH',label:'Chuquisaca'},
+        {value:'LP',label:'La Paz'},
+        {value:'CB',label:'Cochabamba'},
+        {value:'OR',label:'Oruro'},
+        {value:'PT',label:'Potosí'},
+        {value:'TJ',label:'Tarija'},
+        {value:'SC',label:'Santa Cruz'},
+        {value:'BE',label:'Beni'},
+        {value:'PD',label:'Pando'},
+      ],
       users:[],
       usersF:[],
       producer:{},
@@ -505,6 +519,7 @@ export default {
     producerCreate(){
       this.loading=true
       this.producer.user_id=this.store.user.id
+      this.producer.ci=this.producer.ci+' '+ this.departamento.value
       this.$api.post('producer',this.producer).then(res => {
         // this.producerGet()
         this.producers.unshift(res.data)
--- front/src/pages/ProducerShow.vue
+++ front/src/pages/ProducerShow.vue
@@ -14,30 +14,30 @@
             <q-card>
               <q-card-section class="q-py-xs">
                 <div class="row">
-                  <div class="col-6"><q-input dense outlined v-model="producer.nombres" label="NOMBRES(S)" required /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.apellidos" label="APELLIDOS" /></div>
-                  <div class="col-4"><q-input dense outlined v-model="producer.ci" label="CI" /></div>
-                  <div class="col-4"><q-input dense outlined v-model="producer.edad" label="EDAD" /></div>
-                  <div class="col-4"><q-input dense outlined v-model="producer.nroFamilia" label="NO FAMILIA" /></div>
-                  <div class="col-3"><q-input dense outlined v-model="producer.masculino" label="MASCULINO" /></div>
-                  <div class="col-3"><q-input dense outlined v-model="producer.femenino" label="FEMENINO" /></div>
-                  <div class="col-3"><q-input dense outlined v-model="producer.varones" label="VARONES" /></div>
-                  <div class="col-3"><q-input dense outlined v-model="producer.mujeres" label="MUJERES" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.celular" label="CELULAR" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.provincia" label="PROVINCIA" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.departamento" label="1 DEPARTAMENTO" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.municipio" label="2 MUNICIPIO" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.comunidad" label="3 COMUNIDAD" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.superficie" label="4 SUPERFICIE CULTIVADA" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.semilla" label="5 SEMILLA UTILIZADA" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.abono" label="6 TIPO DE ABONO UTILIZADO" /></div>
-                  <div class="col-12"><q-input dense outlined v-model="producer.maquinaria" label="7 MAQUINARIA Y HERRAMIENTAS EMPLEADAS" /></div>
-                  <div class="col-12"><q-input dense outlined v-model="producer.riego" label="8 TIPO DE RIEGO AL QUE TIENE ACCESO" /></div>
-                  <div class="col-12"><q-input dense outlined v-model="producer.quinua" label="9 CANTIDAD DE QUINUA RECOLECTADA POR HECTAREA" /></div>
-                  <div class="col-12"><q-input dense outlined v-model="producer.asociacion" label="10 PERTENECE A ALGUNA ASOCIACIÓN?" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.si" label="SI" /></div>
-                  <div class="col-6"><q-input dense outlined v-model="producer.no" label="NO" /></div>
-                  <div class="col-12"><q-input dense outlined v-model="producer.mejorar" label="11 COMO CREE QUE PODRÍA MEJORAR SU PRODUCCIÓN" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.nombres" label="NOMBRES(S)" required /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.apellidos" label="APELLIDOS" /></div>
+                  <div class="col-4"><q-input disable dense outlined v-model="producer.ci" label="CI" /></div>
+                  <div class="col-4"><q-input disable dense outlined v-model="producer.edad" label="EDAD" /></div>
+                  <div class="col-4"><q-input disable dense outlined v-model="producer.nroFamilia" label="NO FAMILIA" /></div>
+                  <div class="col-3"><q-input disable dense outlined v-model="producer.masculino" label="MASCULINO" /></div>
+                  <div class="col-3"><q-input disable dense outlined v-model="producer.femenino" label="FEMENINO" /></div>
+                  <div class="col-3"><q-input disable dense outlined v-model="producer.varones" label="VARONES" /></div>
+                  <div class="col-3"><q-input disable dense outlined v-model="producer.mujeres" label="MUJERES" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.celular" label="CELULAR" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.provincia" label="PROVINCIA" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.departamento" label="1 DEPARTAMENTO" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.municipio" label="2 MUNICIPIO" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.comunidad" label="3 COMUNIDAD" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.superficie" label="4 SUPERFICIE CULTIVADA" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.semilla" label="5 SEMILLA UTILIZADA" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.abono" label="6 TIPO DE ABONO UTILIZADO" /></div>
+                  <div class="col-12"><q-input disable dense outlined v-model="producer.maquinaria" label="7 MAQUINARIA Y HERRAMIENTAS EMPLEADAS" /></div>
+                  <div class="col-12"><q-input disable dense outlined v-model="producer.riego" label="8 TIPO DE RIEGO AL QUE TIENE ACCESO" /></div>
+                  <div class="col-12"><q-input disable dense outlined v-model="producer.quinua" label="9 CANTIDAD DE QUINUA RECOLECTADA POR HECTAREA" /></div>
+                  <div class="col-12"><q-input disable dense outlined v-model="producer.asociacion" label="10 PERTENECE A ALGUNA ASOCIACIÓN?" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.si" label="SI" /></div>
+                  <div class="col-6"><q-input disable dense outlined v-model="producer.no" label="NO" /></div>
+                  <div class="col-12"><q-input disable dense outlined v-model="producer.mejorar" label="11 COMO CREE QUE PODRÍA MEJORAR SU PRODUCCIÓN" /></div>
                   <div class="col-12 text-center flex flex-center">
                     <q-img :src="url+'../imagenes/'+producer.foto" style="width: 200px; height: 200px;" />
                   </div>
